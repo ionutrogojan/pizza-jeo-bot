f0d306d13d9e5d35c7f59f035b4c422f260d5f5b
Ionut Rogojan <34418533+ionutrogojan@users.noreply.github.com>
2020-06-20 12:29:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,11 +52,11 @@ client.on('message', message => {
 			    }
 					else if(message.content.startsWith(`morning`)){
 			      const taggedUser = message.mentions.users.first();
-			      	message.channel.send(`Good Morning ${taggedUser.username} ! You should have some :pizza: ***pizza***! https://giphy.com/gifs/3orieNTh5JZFPYZXR6 `)
+			      	message.channel.send(`Good Morning ${taggedUser.username}! You should have some :pizza: ***pizza***! https://giphy.com/gifs/3orieNTh5JZFPYZXR6 `)
 			    }
 					else if(message.content.startsWith(`night`)){
 			      const taggedUser = message.mentions.users.first();
-			      	message.channel.send(`Good Night ${taggedUser.username} ! May your dreams be full of :pizza: ***pizza***! https://gph.is/2ILP1Gd `)
+			      	message.channel.send(`Good Night ${taggedUser.username}! May your dreams be full of :pizza: ***pizza***! https://gph.is/2ILP1Gd `)
 			    }
 					else if(message.content.startsWith(`pizza`)){
 			      const taggedUser = message.mentions.users.first();
