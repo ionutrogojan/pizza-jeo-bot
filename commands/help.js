7c3d0c23fda115669c485ed9b7219beb287161e6
Ionut Rogojan <34418533+ionutrogojan@users.noreply.github.com>
2020-07-16 15:13:03 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports = {
       .setColor('#fe9801')
       .setTitle('Pizza Joe commands list')
       .setURL('https://www.youtube.com/watch?v=lpvT-Fciu-4')
-      .setDescription("**HERE ARE THE AVAILABLE COMMANDS**\n Pizza Joe will answer to both `p!` and `Joe`\n \n `p!help` - display commands list\n \n `p!vote` - display vote link to support Pizza Joe\n `p!pizza?` - Pizza Joe will answer\n `p!morning @user` - Pizza Joe will say Morning\n`p!night @user` - Pizza Joe will say Good Night\n `p!pizza @user` - Bake someone a pizza (requires @user)\n `p!joe` - Ping Pizza Joe\n `p!love` - Pizza Joe will love you back\n `Joe I want pizza` - Pizza joe will answer\n `Joe you're a dummy` - Pizza Joe will answer back\n \n **USEFUL LINKS**\n [INVITE ME](https://discord.com/oauth2/authorize?client_id=723293726515921027&permissions=412736&scope=bot) | [SUPPORT](https://discord.com/invite/tEaZ2c7)")
+      .setDescription("**HERE ARE THE AVAILABLE COMMANDS**\n Pizza Joe will answer to both `p!` and `Joe`\n \n `p!help` - display commands list\n \n `p!vote` - Vote link to support Pizza Joe\n `p!update` - Display the current version features and changes\n `p!morning @user` - Pizza Joe will say Morning\n`p!night @user` - Pizza Joe will say Good Night\n `p!pizza @user` - Bake someone a pizza (requires @user)\n `p!joe` - Ping Pizza Joe\n `p!love` - Pizza Joe will love you back\n `Joe Iwantpizza` - Pizza joe will answer\n \n **USEFUL LINKS**\n [INVITE ME](https://discord.com/oauth2/authorize?client_id=723293726515921027&permissions=412736&scope=bot) | [SUPPORT](https://discord.com/invite/tEaZ2c7)")
       .setFooter(`Pizza Joe is the best!`)
     message.channel.send(helpEmbed);
   }
