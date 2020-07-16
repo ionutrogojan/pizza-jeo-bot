26c8130cbe5e0d638a6b25df2c6c57e5aa85ae9a
Ionut Rogojan <34418533+ionutrogojan@users.noreply.github.com>
2020-07-16 15:24:43 +0100

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -8,7 +8,7 @@ module.exports = {
       .setColor('#fe9801')
       .setTitle('Pizza Joe version update')
       //.setURL('https://www.youtube.com/watch?v=lpvT-Fciu-4')
-      .setDescription("**CURRENT VERSION UPDATE**\n \n + Optimized commands\n + Added Pizza Joe to top.gg\n + `p!update` - useful information\n \n - `p!pizza?` -non required anymore\n - `Joe you're a dummy` - Pizza Joe's feelings were hurt\n \n **ROADMAP**\n + pizza count - check how many pizzas you baked\n + Further optimisation of current and future commands\n \n *Thank you everyone who supports and invited Pizza Joe to their servers, it means a lot. New features are comming in the next update.*\n \n **USEFUL LINKS**\n [INVITE ME](https://discord.com/oauth2/authorize?client_id=723293726515921027&permissions=412736&scope=bot) | [SUPPORT](https://discord.com/invite/tEaZ2c7)")
+      .setDescription("**CURRENT VERSION UPDATE**\n \n + Optimized commands\n + Added Pizza Joe to top.gg\n + `p!update` - useful information\n + Pizza Joe will no longer spam the chat when he can't process a command\n \n - `p!pizza?` -non required anymore\n - `Joe you're a dummy` - Pizza Joe's feelings were hurt\n \n **ROADMAP**\n + pizza count - check how many pizzas you baked\n + Further optimisation of current and future commands\n \n *Thank you everyone who supports and invited Pizza Joe to their servers, it means a lot. New features are comming in the next update.*\n \n **USEFUL LINKS**\n [INVITE ME](https://discord.com/oauth2/authorize?client_id=723293726515921027&permissions=412736&scope=bot) | [SUPPORT](https://discord.com/invite/tEaZ2c7)")
       .setFooter(`Pizza Joe is the best!`)
     message.channel.send(helpEmbed);
   }
